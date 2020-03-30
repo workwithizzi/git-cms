60a017846f4c701bb378a620da6efd338bb7a55a
IvaTsu <ivan.tsud@gmail.com>
2020-03-30 22:53:23 +0300

    Add 'sass' and 'post-css' support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
 require("dotenv").config();
 
-module.exports = {
-	// ENV variables
-	env: {
-		GITHUB_PRIVATE_TOKEN: process.env.GITHUB_PRIVATE_TOKEN,
-	},
-};
+const withCSS = require("@zeit/next-css");
+const withSass = require("@zeit/next-sass");
+
+module.exports = withCSS(
+	withSass({
+		// ENV variables
+		env: {
+			GITHUB_PRIVATE_TOKEN: process.env.GITHUB_PRIVATE_TOKEN,
+		},
+		webpack(config, options) {
+			config.module.rules.push({
+				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+				use: {
+					loader: "url-loader",
+					options: {
+						limit: 100000,
+					},
+				},
+			});
+			return config;
+		},
+	})
+);
---
+++ pages/admin.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/admin.scss";
+
+function AdminPage() {
+	return (
+		<h1 className="title">Welcome to the Admin page of the Git CMS</h1>
+	);
+}
+
+export default AdminPage;
