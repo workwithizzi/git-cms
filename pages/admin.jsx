b8b66a4c81da05e8c02543d2e86ea0679c4fd965
IvaTsu <ivan.tsud@gmail.com>
2020-03-31 15:33:35 +0300

    Add 'Font Awesome' icons support

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "../styles/admin.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
+
 function AdminPage() {
 	return (
-		<h1 className="title">Welcome to the Admin page of the Git CMS</h1>
+		<>
+			<h1 className="title">Welcome to the Admin page of the Git CMS</h1>
+
+			<div>
+				<button><FontAwesomeIcon icon={faCog} /> Settings</button>
+			</div>
+		</>
 	);
 }
 
