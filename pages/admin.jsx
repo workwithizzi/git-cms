c1ce592d966b8949213acced6bf9dfa3cc5d93d5
IvaTsu <ivan.tsud@gmail.com>
2020-04-02 13:04:32 +0300

    Add 'limber/pages.yml' fetching and rendering

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -10,28 +10,68 @@ import Drawer from "../components/Drawer";
 import RenderLimberSettings from "../components/RenderLimberSettings";
 
 import RequestService from "../util/requestService";
+import parseYaml from "../util/parseYaml";
 
 function AdminPage({ router, limberSettings }) {
 
 	// PARSE "limber/settings.yml" content as JS Object and PUT to the State BEFORE on page render
 	useEffect(() => {
 		_parseSettings(limberSettings);
+		_parsePages();
 	}, []);
 
 	// DEFINE State
 	const [settings, setSettings] = useState({});
+	const [pagesSettings, setPagesSettings] = useState({});
 
 	// PARSE "limber/settings.yml" content as JS Object and PUT to the State
-	async function _parseSettings(settings) {
-		const settingsFileContent = settings.content;
-		const fromBase64ToString = window.atob(settingsFileContent);
-		setSettings(jsyaml.load(fromBase64ToString));
+	function _parseSettings(settings) {
+		const data = parseYaml(settings);
+		setSettings(data);
+	}
+
+	async function _parsePages() {
+		// it is not wraped in a try-catch block as RequestService has it's own error handling
+		const response = await RequestService
+			.getLimberPagesSettings({
+				auth: {
+					username: process.env.GITHUB_PRIVATE_TOKEN,
+				},
+			});
+		const data = parseYaml(response);
+		setPagesSettings(data);
+	}
+
+	function _fetchPages(path) {
+		console.log(path);
+	}
+
+	function _renderContentGroups(groups) {
+		return groups.map(group => {
+			if (router.asPath === `/admin?page=${group.name}`) {
+				return (
+					<div key={Math.random(100)}>
+						{
+							group.label && <h1>{group.label}</h1>
+						}
+						{
+							group.description && <h3>{group.description}</h3>
+						}
+						{
+							group.name === pagesSettings.name && _fetchPages(pagesSettings.path)
+						}
+					</div>
+				);
+			}
+		});
 	}
 
 	return (
 		<>
 			<Layout>
 				<Drawer activePath={router.asPath} limberData={settings} />
+
+				{/* Dashboard tab */}
 				{
 					router.asPath === "/admin" && (
 						<>
@@ -40,12 +80,18 @@ function AdminPage({ router, limberSettings }) {
 					)
 				}
 
+				{/* Settings tab */}
 				{
 					router.asPath === "/admin?page=settings" && (
 						<>
 							<RenderLimberSettings settings={settings} />
 						</>
 					)
+				}
+
+				{/* Content-Groups tabs */}
+				{
+					settings.groups && _renderContentGroups(settings.groups)
 				}
 			</Layout>
 		</>
