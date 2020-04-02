2986d2834cad55149a245d347ed7d63c67e65289
IvaTsu <ivan.tsud@gmail.com>
2020-04-02 10:51:22 +0300

    Render 'Pages' and 'Posts' as the menu items

--- components/Drawer/index.jsx
+++ components/Drawer/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCog } from "@fortawesome/free-solid-svg-icons";
+import { faTachometerAlt, faCog, faFileAlt, faFile, faRandom } from "@fortawesome/free-solid-svg-icons";
 import PT from "prop-types";
 
 function Drawer({ activePath, limberData }) {
@@ -15,11 +15,36 @@ function Drawer({ activePath, limberData }) {
 		data && setIsLimberSettings(true);
 	}
 
+	function _renderLimberContentGroups(groups) {
+		return (
+			groups.map(group => {
+
+				let icon;
+				if (group.name === "pages") {
+					icon = faFileAlt;
+				} else if (group.name === "posts") {
+					icon = faFile;
+				} else {
+					icon = faRandom;
+				}
+
+				return (
+					<li className="l-drawer__item" key={Math.random(100)}>
+						<a className={`l-drawer__btn btn--content-group ${activePath === `/admin?page=${group.name}` ? "isActive" : "notActive"}`} href={`/admin?page=${group.name}`}>
+							<FontAwesomeIcon className="btn--icon content-group--icon" icon={icon} />
+							<span className="btn--text content-group--text">{group.label}</span>
+						</a>
+					</li>
+				);
+			})
+		);
+	}
+
 	return (
 		<ul className="l-drawer">
 			<li className="l-drawer__item">
 				<a className={`l-drawer__btn btn--dashboard ${activePath === "/admin" ? "isActive" : "notActive"}`} href="/admin">
-					<FontAwesomeIcon className="btn--icon dashboard--icon" icon={faCog} />
+					<FontAwesomeIcon className="btn--icon dashboard--icon" icon={faTachometerAlt} />
 					<span className="btn--text dashboard--text">Dashboard</span>
 				</a>
 			</li>
@@ -31,6 +56,11 @@ function Drawer({ activePath, limberData }) {
 							<span className="btn--text settings--text">Settings</span>
 						</a>
 					</li>
+				)
+			}
+			{
+				isLimberSettings && limberData.groups.length >= 1 && (
+					_renderLimberContentGroups(limberData.groups)
 				)
 			}
 		</ul>
