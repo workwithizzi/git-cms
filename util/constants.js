d61586bd68d85b30e82bd67ff87539bf81d2dc8a
IvaTsu <ivan.tsud@gmail.com>
2020-04-02 13:05:50 +0300

    Add 'limber/pages.yml' global path

--- util/constants.js
+++ util/constants.js
@@ -3,4 +3,5 @@ module.exports = {
 	GITHUB_REPO_OWNER: "workwithizzi",
 	GITHUB_REPO: "git-cms-test",
 	GITHUB_LIMBER_SETTINGS_PATH: "limber/settings.yml",
+	GITHUB_LIMBER_PAGES_SETTINGS_PATH: "limber/pages.yml",
 };
